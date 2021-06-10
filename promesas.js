03f14b5ca6d26ebc2c717268746d1fc1f364914c
pablohariq <lizamav.p@gmail.com>
2021-06-10 12:19:24 -0400

    desafio completado

---
+++ promesas.js
@@ -0,0 +1,29 @@
+const obtenerAlbumesPorId =  async (albumId) =>{
+    const url = "https://jsonplaceholder.typicode.com/photos";
+    try{
+        const response = await fetch(url)
+        const albumes = await response.json() //todos los albumes, son 5000
+        // console.log(albumes)
+        const AlbumesSegunAlbumId = albumes.filter(a => a.albumId == albumId) //todos los albumes del AlbumId solicitado
+        // console.log(veinteAlbumesSegunAlbumId)
+        const veinteAlbumesSegunAlbumId = AlbumesSegunAlbumId.slice(0,20); //el slice es de la forma [a,b)
+        console.log(veinteAlbumesSegunAlbumId)
+    }
+    catch(e){
+        console.log(e)
+    }
+}
+
+obtenerAlbumesPorId(2)
+
+const mensajeInformacion = () =>{
+    return new Promise((resolve,reject) =>{
+        setTimeout(()=>{resolve("Información Enviada")},3000)
+    })
+}
+
+//segun lo conversado en clases con Brian entregaremos el mensaje a través de una IIFE
+(async () => {
+    const mensajeDePromesaResuelta = await mensajeInformacion()
+    console.log(mensajeDePromesaResuelta)
+})()
